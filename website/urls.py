c38f379436d91cee49eb16bd50ff04fdb8020fc2
yasamanhatami <152697252+yasamanhatami@users.noreply.github.com>
2024-03-21 21:07:15 +0330

    I created my cv

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from website.views import *
+app_name='website'
+urlpatterns = [
+    path('',index_views,name='index'),
+    path('about/',about_views,name='about'),
+    path('contact/',contact_views,name='contact'),
+
+]
---
+++ website/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index_views (request):
+    return render(request,'website/index.html')
+def about_views (request):
+    return render(request,'website/about.html')
+def contact_views (request):
+    return render(request,'website/contact.html')
+
+
+# Create your views here.
