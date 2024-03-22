55193f3a3d862eca3da0f8bf73de59743f8f874c
yasamanhatami <152697252+yasamanhatami@users.noreply.github.com>
2024-03-22 22:02:14 +0330

    Modify the project
    I created a base.html file and inherited from the templates and passed the information through the renderer, then removed the lorm texts and removed the service part.

--- website/views.py
+++ website/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index_views (request):
-    return render(request,'website/index.html')
-def about_views (request):
-    return render(request,'website/about.html')
-def contact_views (request):
-    return render(request,'website/contact.html')
+    context = {'Birthday':'16 Octuber 2008',
+               'Website':'www.example.com',
+               'Phone':'+123 456 7890',
+               'City':'kerman,Iran',
+               'Age':'15',
+               'Degree':'student',
+               'PhEmailone':'yasamanhatami1387@gmail.com',
+               'Language':'English,Persian'}
+    return render(request,'website/index.html',context)
+
 
 
 # Create your views here.
